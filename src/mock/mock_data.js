eef0b180053edd30edb337be353de334bfc765f6
GongHaiLink <38188996+gonghailink@users.noreply.github.com>
2025-10-19 01:10:21 +0800

    chore: 更新导航数据 - 2025/10/19 01:10:28

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -60,7 +60,7 @@ export const mockData = {
           "name": "CF-TTS",
           "url": "https://ttsapi.pages.dev/",
           "description": "兼容OpenAI API格式接口的文本转语音（TTS）代理服务。",
-          "icon": "https://media.istockphoto.com/id/1193300702/vector/sound-icon-flat-volume-sign-symbol-music-player-sticker.jpg?s=612x612&w=0&k=20&c=dkM4qLGJZCiKL8TjdVB0-ZbKIoKkMRBHVN2002LbpWQ="
+          "icon": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9B4F3mHF_KXX6chACw5LUwo0UJ5Pe868X-g&s"
         },
         {
           "id": "site-1760806569570",
