e6d353cebb90f317db83b8a51806c6ee31aa1a78
GongHaiLink <38188996+gonghailink@users.noreply.github.com>
2025-10-20 17:56:54 +0800

    chore: 更新导航数据 - 2025/10/20 17:57:06

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -104,6 +104,13 @@ export const mockData = {
           "url": "https://www.browserscan.net/",
           "description": "DNS泄漏检测",
           "icon": "https://moqingtk.com/wp-content/uploads/2024/08/1724036974-BrowserScanlogo500px.webp"
+        },
+        {
+          "id": "site-1760954223888",
+          "name": "ITDOG",
+          "url": "https://www.itdog.cn/tcping",
+          "description": "ITDOG在线tcping",
+          "icon": "https://www.urlsn.com/wp-content/uploads/1743850537334.svg"
         }
       ]
     },
